a9009424628f5c41ae964567c6b48701599e64e9
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-02 12:39:01 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -975,7 +975,7 @@ while True:
 							result = wks.acell(basicSetting[16]).value
 
 							embed = discord.Embed(
-									description= '```' + SearchID + ' 님이 받을 다이야는 ' + result + ' 다이야 입니다.```',
+									description= '```' + SearchID + ' 모델명 코드는 ' + result + ' 입니다.```',
 									color=0xff00ff
 									)
 							await msg.channel.send(embed=embed, tts=False)
@@ -1905,7 +1905,7 @@ while True:
 					result = wks.acell(basicSetting[16]).value
 
 					embed = discord.Embed(
-							description= '```' + SearchID + ' 님이 받을 다이야는 ' + result + ' 다이야 입니다.```',
+							description= '```' + SearchID + ' 모델명 코드는 ' + result + ' 입니다.```',
 							color=0xff00ff
 							)
 					await msg.channel.send(embed=embed, tts=False)
